3ca5288855bd28205a5c0266a23ac1e5da346bfd
patrickm765 <150442874+patrickm765@users.noreply.github.com>
2024-02-12 12:41:40 +0800

    first update

---
+++ # Question 5.py
@@ -0,0 +1,13 @@
+# Question 5
+
+import tkinter as tk
+from PIL import ImageTk, Image
+
+root = tk.Tk()
+root.title("Final Exam Question 5") #opens tkinter window
+
+
+
+
+
+root.mainloop() #closes tkinter
